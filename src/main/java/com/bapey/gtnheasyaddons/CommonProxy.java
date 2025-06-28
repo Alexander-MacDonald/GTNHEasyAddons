0bd981289a772dc359f557f13dc0d4399d7914dd
Alexander MacDonald <92743245+Alexander-MacDonald@users.noreply.github.com>
2025-06-28 09:25:11 -0400

    Tiers added- note on assembler compression

--- src/main/java/com/bapey/gtnheasyaddons/CommonProxy.java
+++ src/main/java/com/bapey/gtnheasyaddons/CommonProxy.java
@@ -17,18 +17,202 @@ public class CommonProxy {
         Config.synchronizeConfiguration(event.getSuggestedConfigurationFile());
 
         MyMod.LOG.info(Config.greeting);
-        MyMod.LOG.info("I am MyMod at version " + Tags.VERSION);
+        MyMod.LOG.info("I am GTNHEasyAddons at version " + Tags.VERSION);
     }
 
     // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
     public void init(FMLInitializationEvent event) {
-        ItemStack[] inputs = { new ItemStack(Blocks.gravel), new ItemStack(Blocks.dirt) };
+        ItemStack[][] inputs = {
+            // Default
+            { new ItemStack(Blocks.gravel), new ItemStack(Blocks.dirt) },
+            // Basic Stone Age
+            {},
+            // Steam Age
+            // ULV
+            // Tier 1 LV
+            // Tier 2 MV
+            // Tier 3 HV
+            // Tier 4 MV
+            // Tier 5 IV
+            // Tier 6 LUV
+            // Tier 7 ZPM
+            // Tier 8 UV
+            // Tier 9 UHV
+            // Magic Novice
+            // Magic Adept
+            // Magic Master
+            // Magic Grandmaster
+            // Magic Grandmaster Unique
+            // Legendary
+            // Legenary Unique
+            // BM Novice
+            // BM Adept
+            // BM Master
+            // Forest Ranger Basic
+            // Forest Ranger Advanced
+            // Forest Ranger Expert
+            // Space Invaders Moon
+            // Space Invaders Mars
+            // Space Invaders Asteroids
+            // Bees Basic
+            // Bees Advanced
+            // Bees Expert
+            // Fast Food
+            // Slow Food
+            // Haute Cuisine
+            // Dessert
+            // Transportation 3 Class
+            // Transportation 2 Class
+            // Transportation 1 Class
+            // Seeds
+            // Gardens
+            // AE Basics
+            // AE Good
+            // AE Advanced
+            // AE Master
+            // Computer Floppies
+            // Tier 1 Computers
+            // Tier 2 Computers
+            // Tier 3 Computers
+            // Tier 3.5 Computers
+            // Tier 4 Computers
+            // IC2 Crops
+            // Witchery Novice
+            // Witchery Adept
+            // Witchery Master
+            // Witchery Witch
+            // Witchery Deamon
+            // HEE-Basic
+            // HEE-Advanced
 
-        ItemStack[] bags = { new ItemStack(GameRegistry.findItem("enhancedlootbags", "lootbag"), 1, 0),
-            new ItemStack(GameRegistry.findItem("enhancedlootbags", "lootbag"), 1, 2) };
+        };
 
+        ItemStack[] bags = {
+            // Default
+            new ItemStack(GameRegistry.findItem("enhancedlootbags", "lootbag"), 1, 0),
+            // Stone Age
+            new ItemStack(GameRegistry.findItem("enhancedlootbags", "lootbag"), 1, 2)
+            // Steam Age
+            // ULV
+            // Tier 1 LV
+            // Tier 2 MV
+            // Tier 3 HV
+            // Tier 4 MV
+            // Tier 5 IV
+            // Tier 6 LUV
+            // Tier 7 ZPM
+            // Tier 8 UV
+            // Tier 9 UHV
+            // Magic Novice
+            // Magic Adept
+            // Magic Master
+            // Magic Grandmaster
+            // Magic Grandmaster Unique
+            // Legendary
+            // Legenary Unique
+            // BM Novice
+            // BM Adept
+            // BM Master
+            // Forest Ranger Basic
+            // Forest Ranger Advanced
+            // Forest Ranger Expert
+            // Space Invaders Moon
+            // Space Invaders Mars
+            // Space Invaders Asteroids
+            // Bees Basic
+            // Bees Advanced
+            // Bees Expert
+            // Fast Food
+            // Slow Food
+            // Haute Cuisine
+            // Dessert
+            // Transportation 3 Class
+            // Transportation 2 Class
+            // Transportation 1 Class
+            // Seeds
+            // Gardens
+            // AE Basics
+            // AE Good
+            // AE Advanced
+            // AE Master
+            // Computer Floppies
+            // Tier 1 Computers
+            // Tier 2 Computers
+            // Tier 3 Computers
+            // Tier 3.5 Computers
+            // Tier 4 Computers
+            // IC2 Crops
+            // Witchery Novice
+            // Witchery Adept
+            // Witchery Master
+            // Witchery Witch
+            // Witchery Deamon
+            // HEE-Basic
+            // HEE-Advanced
+        };
+        // Default
         GameRegistry.addShapedRecipe(bags[0], "XXX", "XXX", "XXX", 'X', inputs[0]);
+        // Stone Age
         GameRegistry.addShapedRecipe(bags[1], "XXX", "XXX", "XXX", 'X', inputs[1]);
+        // Steam Age
+        // ULV
+        // Tier 1 LV
+        // Tier 2 MV
+        // Tier 3 HV
+        // Tier 4 MV
+        // Tier 5 IV
+        // Tier 6 LUV
+        // Tier 7 ZPM
+        // Tier 8 UV
+        // Tier 9 UHV
+        // Magic Novice
+        // Magic Adept
+        // Magic Master
+        // Magic Grandmaster
+        // Magic Grandmaster Unique
+        // Legendary
+        // Legenary Unique
+        // BM Novice
+        // BM Adept
+        // BM Master
+        // Forest Ranger Basic
+        // Forest Ranger Advanced
+        // Forest Ranger Expert
+        // Space Invaders Moon
+        // Space Invaders Mars
+        // Space Invaders Asteroids
+        // Bees Basic
+        // Bees Advanced
+        // Bees Expert
+        // Fast Food
+        // Slow Food
+        // Haute Cuisine
+        // Dessert
+        // Transportation 3 Class
+        // Transportation 2 Class
+        // Transportation 1 Class
+        // Seeds
+        // Gardens
+        // AE Basics
+        // AE Good
+        // AE Advanced
+        // AE Master
+        // Computer Floppies
+        // Tier 1 Computers
+        // Tier 2 Computers
+        // Tier 3 Computers
+        // Tier 3.5 Computers
+        // Tier 4 Computers
+        // IC2 Crops
+        // Witchery Novice
+        // Witchery Adept
+        // Witchery Master
+        // Witchery Witch
+        // Witchery Deamon
+        // HEE-Basic
+        // HEE-Advanced
+        
+        //TODO: Overwrite Assembler Compression Recipes in Vanillas GTNH
     }
 
     // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
--- src/main/java/com/bapey/gtnheasyaddons/Tags.java
+++ src/main/java/com/bapey/gtnheasyaddons/Tags.java
@@ -3,5 +3,5 @@ package com.bapey.gtnheasyaddons;
 public class Tags {
 
     public static final String MODID = "gtnheasyaddons";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.3";
 }
